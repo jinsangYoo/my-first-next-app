5238845267d9b7c920f143e92c0834be5639c4e8
jinsang <jinsang@nhndata.com>
2023-04-18 16:53:23 +0900

    useRouter 훅을 이용한 파라미터 얻기

--- src/pages/posts/greet/[name].tsx
+++ src/pages/posts/greet/[name].tsx
@@ -1,6 +1,7 @@
 import {GetServerSideProps} from 'next'
 import {GreetParams} from '@/pages/types/greet-params'
 import {FC} from 'react'
+import {useRouter} from 'next/router'
 
 export const getServerSideProps: GetServerSideProps<GreetParams> = async ({params}) => {
   const {name} = params as GreetParams
@@ -11,12 +12,18 @@ export const getServerSideProps: GetServerSideProps<GreetParams> = async ({param
   }
 }
 
-interface GreetProps {
-  name?: string
-}
+// interface GreetProps {
+//   name?: string
+// }
 
-const Greet: FC<GreetProps> = props => {
-  return <h1>Hello, {props.name}!</h1>
+// const Greet: FC<GreetProps> = props => {
+//   return <h1>Hello, {props.name}!</h1>
+// }
+
+const Greet = () => {
+  const {query} = useRouter()
+  console.log(query)
+  return <h1>Hello, {query.name}!</h1>
 }
 
 export default Greet
