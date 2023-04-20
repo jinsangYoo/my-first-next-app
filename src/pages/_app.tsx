0843a1d4675713963170da21d64ea831930f1394
jinsang <jinsang@nhndata.com>
2023-04-20 16:56:59 +0900

    _app 파일 커스텀까지

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,38 @@
+import Navbar from '@/components/navbar'
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {useState} from 'react'
+import ThemeContext from '@/components/themeContext'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const themes = {
+  dark: {
+    background: 'black',
+    color: 'white',
+  },
+  light: {
+    background: 'light',
+    color: 'black',
+  },
+}
+
+export default function App({Component, pageProps}: AppProps) {
+  const [theme, setTheme] = useState('light')
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+    return null
+  }
+
+  return (
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+      <div
+        style={{
+          width: '100%',
+          minHeight: '100vh',
+          ...themes[theme],
+        }}>
+        <Navbar />
+        <Component {...pageProps} />
+      </div>
+    </ThemeContext.Provider>
+  )
 }
